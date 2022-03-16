deeb7e9f2e0739948b9aeee21f13758aeca1acda
EchauriC <52312286+EchauriC@users.noreply.github.com>
2022-03-16 14:58:11 +0100

    Cambio de UI
    Cambio de editor visual de UI a generación por codigo.

---
+++ src/Stat.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Stat {
+    int key;
+    String value;
+    boolean principal;
+
+    public Stat(int key, String value, boolean principal) {
+        this.key = key;
+        this.value = value;
+        this.principal = principal;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public boolean isPrincipal() {
+        return principal;
+    }
+
+    public void setPrincipal(boolean principal) {
+        this.principal = principal;
+    }
+    
+    
+}
---
+++ src/StatsParser.java
@@ -0,0 +1,76 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class StatsParser {
+    
+    String[] principales;
+    String[] secundarios;
+
+    public String[] getPrincipales() {
+        return principales;
+    }
+
+    public void setPrincipales(String[] principales) {
+        this.principales = principales;
+    }
+
+    public String[] getSecundarios() {
+        return secundarios;
+    }
+
+    public void setSecundarios(String[] secundarios) {
+        this.secundarios = secundarios;
+    }
+    
+    public Map<Integer, String> getPrincipalesAsMap(){
+        Map<Integer, String> map =  new HashMap<>();
+        int i = 0;
+        for (String item : principales){
+            map.put(i, item);
+            i++;
+        }
+        return map;
+    }
+    
+    public Map<Integer, String> getSecundariosAsMap(){
+        Map<Integer, String> map =  new HashMap<>();
+        int i = 0;
+        for (String item : secundarios){
+            map.put(i, item);
+            i++;
+        }
+        return map;
+    }
+    
+    public ArrayList<Stat> getPrincipalesAsStatArrayList(){
+        ArrayList<Stat> statsObjects = new ArrayList();
+        int i = 0;
+        for (String item : principales){
+            statsObjects.add(new Stat(i, item, true));
+            i++;
+        }
+        return statsObjects;
+    }
+    
+    public ArrayList<Stat> getSecundariosAsStatArrayList(){
+        ArrayList<Stat> statsObjects = new ArrayList();
+        int i = 0;
+        for (String item : secundarios){
+            statsObjects.add(new Stat(i, item, false));
+            i++;
+        }
+        return statsObjects;
+    }
+}
